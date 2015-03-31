4cba9131b65d1bc253214e9d8a4ddf795c0ccdb4
Anthony Mao <whs.fbla.amao@gmail.com>
2015-03-30 21:59:55 -0700

    heroku FINALLY WORKS!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Skillit' });
+});
+
+
+/* GET secret page. */
+router.get('/hiddenLeaf', function(req, res, next) {
+    res.render('hiddenVillage', { title: 'Konoha' });
 });
 
 module.exports = router;
